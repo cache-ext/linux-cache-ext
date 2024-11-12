7e6aea4495065100dcfea652e8ef6f31cb063cdd
Tal Zussman <tz2294@columbia.edu>
2024-11-11 22:33:28 -0700

    bench_leveldb: Remove unused code

--- testing/page_cache_ext/bench_leveldb.py
+++ testing/page_cache_ext/bench_leveldb.py
@@ -2,13 +2,10 @@ import os
 import re
 import logging
 import argparse
-import subprocess
-
 from time import sleep
-from bench_lib import *
 from typing import Dict, List
-from ruamel.yaml import YAML
-from contextlib import suppress, contextmanager
+
+from bench_lib import *
 
 
 log = logging.getLogger(__name__)
@@ -106,44 +103,6 @@ def parse_leveldb_bench_results(stdout: str) -> Dict:
     ):
         raise Exception("Could not parse results from stdout: \n" + stdout)
     return results
-
-
-class CacheExtPolicy:
-    def __init__(self, cgroup: str, loader_path: str, watch_dir: str):
-        self.cgroup_path = "/sys/fs/cgroup/%s" % cgroup
-        self.loader_path = loader_path
-        self.watch_dir = watch_dir
-        self.has_started = False
-        self._policy_thread = None
-
-    def start(self):
-        if self.has_started:
-            raise Exception("Policy already started")
-        self.has_started = True
-        cmd = ["sudo", self.loader_path, "--watch_dir", self.watch_dir]
-        log.info("Starting policy thread: %s", cmd)
-        self._policy_thread = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        sleep(2)
-        if self._policy_thread.poll() is not None:
-            raise Exception(
-                "Policy thread exited unexpectedly: %s"
-                % self._policy_thread.stderr.read()
-            )
-
-    def stop(self):
-        if not self.has_started:
-            raise Exception("Policy not started")
-        cmd = ["sudo", "kill", "-2", str(self._policy_thread.pid)]
-        run(cmd)
-        out, err = self._policy_thread.communicate()
-        with suppress(subprocess.CalledProcessError):
-            run(["sudo", "rm", "/sys/fs/bpf/cache_ext/scan_pids"])
-        log.info("Policy thread stdout: %s", out)
-        log.info("Policy thread stderr: %s", err)
-        self.has_started = False
-        self._policy_thread = None
 
 
 class LevelDBBenchmark(BenchmarkFramework):
