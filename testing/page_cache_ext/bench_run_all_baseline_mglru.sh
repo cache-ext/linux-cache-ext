0e29b5a3f9d5a359e5f2b9324b837a65e4624a61
Tal Zussman <tz2294@columbia.edu>
2024-11-30 18:35:23 -0700

    Bench updates

--- testing/page_cache_ext/bench_run_all_baseline_mglru.sh
+++ testing/page_cache_ext/bench_run_all_baseline_mglru.sh
@@ -7,27 +7,37 @@ set -u
 
 # Microbenchmark with fio for CPU overhead
 # Assumes the data disk is /dev/sdb
-python3 bench_fio.py \
-    --cpu 8 \
-    --target-file /dev/sdb \
-    --results-file ./baseline_mglru_fio_results.json
+# python3 bench_fio.py \
+#     --cpu 8 \
+#     --target-file /dev/sdb \
+#     --results-file ./baseline_mglru_fio_results.json
 
 # Benchmark LevelDB with mixed GET-SCAN
-python3 bench_leveldb.py \
-    --cpu 8 \
-    --policy-loader ./page_cache_ext_get_scan.out \
-    --results-file ./baseline_mglru_get_scan_results.json \
-    --leveldb-db /mydata/leveldb_db \
-    --benchmark mixed_get_scan
+# python3 bench_leveldb.py \
+#     --cpu 8 \
+#     --policy-loader ./page_cache_ext_get_scan.out \
+#     --results-file ./baseline_mglru_get_scan_results.json \
+#     --leveldb-db /mydata/leveldb_db \
+#     --benchmark mixed_get_scan
 
 # Benchmark LevelDB with LFU
 python3 bench_leveldb.py \
     --cpu 8 \
     --policy-loader ./page_cache_ext_sampling.out \
-    --results-file ./baseline_mglru_lfu_ycsb_results.json \
-    --leveldb-db /mydata/leveldb_db \
+    --results-file ./results/baseline_mglru_ycsb_results.json \
+    --leveldb-db /mydata/leveldb_myycsb_db \
     --fadvise-hints "" \
-    --benchmark ycsb_a,ycsb_b,ycsb_c
+    --benchmark uniform,uniform_read_write #ycsb_a,ycsb_b,ycsb_c,ycsb_d,ycsb_f
+
+# Benchmark Twitter traces
+# for cluster in 17 18 34 52; do
+# 	python3 bench_twitter_trace.py \
+#         --cpu 8 \
+#         --policy-loader ./page_cache_ext_sampling.out \
+#         --results-file ./results/baseline_mglru_twitter_traces_${cluster}.json \
+#         --leveldb-db /mydata/leveldb_twitter_cluster${cluster}_db \
+#         --benchmark twitter_cluster${cluster}_bench
+# done
 
 # Benchmark Google IO traces
 # TRACE_DIR="google_traces/data_cluster1_16TB_20240115_data-00000-of-00100"
@@ -38,8 +48,8 @@ python3 bench_leveldb.py \
 #     --trace-data-dir $TRACE_DIR
 
 # Benchmark filesearch workload with MRU
-python3 bench_filesearch.py \
-    --cpu 8 \
-    --policy-loader ./page_cache_ext_mru.out \
-    --results-file ./baseline_mglru_filesearch_results.json  \
-    --data-dir /mydata/filesearch_data
+# python3 bench_filesearch.py \
+#     --cpu 8 \
+#     --policy-loader ./page_cache_ext_mru.out \
+#     --results-file ./baseline_mglru_filesearch_results.json  \
+#     --data-dir /mydata/filesearch_data
--- testing/page_cache_ext/bench_run_twitter_traces_cache_ext.sh
+++ testing/page_cache_ext/bench_run_twitter_traces_cache_ext.sh
@@ -8,29 +8,29 @@ set -u
 cd /mydata/linux/linux/testing/page_cache_ext
 
 #for cluster in 3 4 5 6 7 8 17 18 24 34 52; do
-for cluster in 17 18 24 34 52; do
-	python3 bench_twitter_trace.py \
-        --cpu 8 \
-        --policy-loader ./cache_ext_s3fifo.out \
-        --results-file ./cache_ext_twitter_traces_${cluster}.json \
-        --leveldb-db /mydata/leveldb_twitter_cluster${cluster}_db \
-        --benchmark twitter_cluster${cluster}_bench
-done
+# for cluster in 17 18 24 34 52; do
+# 	python3 bench_twitter_trace.py \
+#         --cpu 8 \
+#         --policy-loader ./cache_ext_s3fifo.out \
+#         --results-file ./results/cache_ext_twitter_traces_${cluster}.json \
+#         --leveldb-db /mydata/leveldb_twitter_cluster${cluster}_db \
+#         --benchmark twitter_cluster${cluster}_bench
+# done
 
-for cluster in 17 18 24 34 52; do
-    python3 bench_twitter_trace.py \
-        --cpu 8 \
-        --policy-loader ./cache_ext_lhd.out \
-        --results-file ./cache_ext_twitter_traces_${cluster}.json \
-        --leveldb-db /mydata/leveldb_twitter_cluster${cluster}_db \
-        --benchmark twitter_cluster${cluster}_bench
-done
+# for cluster in 17 18 24 34 52; do
+#     python3 bench_twitter_trace.py \
+#         --cpu 8 \
+#         --policy-loader ./cache_ext_lhd.out \
+#         --results-file ./results/cache_ext_twitter_traces_${cluster}.json \
+#         --leveldb-db /mydata/leveldb_twitter_cluster${cluster}_db \
+#         --benchmark twitter_cluster${cluster}_bench
+# done
 
 for cluster in 17 18 24 34 52; do
     python3 bench_twitter_trace.py \
         --cpu 8 \
         --policy-loader ./page_cache_ext_sampling.out \
-        --results-file ./cache_ext_twitter_traces_${cluster}.json \
+        --results-file ./results/fixed_cache_ext_twitter_traces_${cluster}.json \
         --leveldb-db /mydata/leveldb_twitter_cluster${cluster}_db \
         --benchmark twitter_cluster${cluster}_bench
 done
