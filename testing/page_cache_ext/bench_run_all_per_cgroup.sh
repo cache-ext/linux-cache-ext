94c47a155a978a0d7c07d8875fa7f3137fc74c2a
Tal Zussman <tz2294@columbia.edu>
2024-12-02 01:18:56 -0700

    Additional per-cgroup bench tweaks

--- testing/page_cache_ext/bench_run_all_per_cgroup.sh
+++ testing/page_cache_ext/bench_run_all_per_cgroup.sh
@@ -22,29 +22,40 @@ python3 bench_per_cgroup.py \
     --data-dir /mydata/cgroup/linuxes/linux \
     --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
     --second-policy-loader ./page_cache_ext_sampling_per_cgroup.out \
-    --results-file ./results/per_cgroup_both_lfu_results.json \
+    --results-file ./results/per_cgroup_baseline_results.json \
     --leveldb-db /mydata/leveldb_myycsb_db \
     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
     --benchmark ycsb_c
 
-python3 bench_per_cgroup.py \
-    --cpu 8 \
-    --search-path /mydata/cgroup \
-    --data-dir /mydata/cgroup/linuxes/linux \
-    --policy-loader ./page_cache_ext_mru_per_cgroup.out \
-    --second-policy-loader ./page_cache_ext_mru_per_cgroup.out \
-    --results-file ./results/per_cgroup_both_mru_results.json \
-    --leveldb-db /mydata/leveldb_myycsb_db \
-    --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
-    --benchmark ycsb_c
+# python3 bench_per_cgroup.py \
+#     --cpu 8 \
+#     --search-path /mydata/cgroup \
+#     --data-dir /mydata/cgroup/linuxes/linux \
+#     --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+#     --second-policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+#     --results-file ./results/per_cgroup_both_lfu_results.json \
+#     --leveldb-db /mydata/leveldb_myycsb_db \
+#     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+#     --benchmark ycsb_c
 
-python3 bench_per_cgroup.py \
-    --cpu 8 \
-    --search-path /mydata/cgroup \
-    --data-dir /mydata/cgroup/linuxes/linux \
-    --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
-    --second-policy-loader ./page_cache_ext_mru_per_cgroup.out \
-    --results-file ./results/per_cgroup_split_results.json \
-    --leveldb-db /mydata/leveldb_myycsb_db \
-    --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
-    --benchmark ycsb_c
+# python3 bench_per_cgroup.py \
+#     --cpu 8 \
+#     --search-path /mydata/cgroup \
+#     --data-dir /mydata/cgroup/linuxes/linux \
+#     --policy-loader ./page_cache_ext_mru_per_cgroup.out \
+#     --second-policy-loader ./page_cache_ext_mru_per_cgroup.out \
+#     --results-file ./results/per_cgroup_both_mru_results.json \
+#     --leveldb-db /mydata/leveldb_myycsb_db \
+#     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+#     --benchmark ycsb_c
+
+# python3 bench_per_cgroup.py \
+#     --cpu 8 \
+#     --search-path /mydata/cgroup \
+#     --data-dir /mydata/cgroup/linuxes/linux \
+#     --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+#     --second-policy-loader ./page_cache_ext_mru_per_cgroup.out \
+#     --results-file ./results/per_cgroup_split_results.json \
+#     --leveldb-db /mydata/leveldb_myycsb_db \
+#     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+#     --benchmark ycsb_c
