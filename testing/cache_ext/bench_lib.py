e13e8e4d1b9098588dcdec9073c6df786a3851fc
Tal Zussman <tz2294@columbia.edu>
2025-08-02 18:46:29 -0400

    bench: Support per-cgroup bench framework

--- testing/cache_ext/bench_lib.py
+++ testing/cache_ext/bench_lib.py
@@ -32,6 +32,7 @@ class CacheExtPolicy:
     def start(self, cgroup_size: int=0):
         if self.has_started:
             raise Exception("Policy already started")
+
         self.has_started = True
         cmd = [
             "sudo",
@@ -393,12 +394,17 @@ class BenchmarkFramework(ABC):
         else:
             self.args = self.parse_args()
 
+        self.second_command = False
+
     def benchmark_prepare(self, config):
         pass
 
     @abstractmethod
     def benchmark_cmd(self, config):
         raise NotImplementedError
+
+    def second_benchmark_cmd(self, config):
+        return []
 
     def cmd_extra_envs(self, config):
         return {}
@@ -513,10 +519,22 @@ class BenchmarkFramework(ABC):
                 log.info("Adding extra envs: %s" % extra_envs)
             env.update(extra_envs)
             self.before_benchmark(config)
-            log.info("Running command: %s" % cmd)
             try:
+                if self.second_command:
+                    second_cmd = self.second_benchmark_cmd(config)
+                    log.info("Running second command: %s" % second_cmd)
+                    second_proc = subprocess.Popen(second_cmd, stdout=subprocess.PIPE)
+
+                log.info("Running command: %s" % cmd)
                 stdout = run_command_with_live_output(cmd, env=env)
                 # stdout = check_output(cmd, encoding="utf-8", env=env)
+
+                if self.second_command:
+                    ret_code = second_proc.wait()
+                    if ret_code != 0:
+                        log.error("Second benchmark failed with error code %s" % ret_code)
+                        raise CalledProcessError(ret_code, self.second_benchmark_cmd(config))
+                    second_proc_output = second_proc.stdout.read().decode("utf-8")
             except CalledProcessError as e:
                 log.error("Benchmark failed with error code %s" % e.returncode)
                 log.error("Output was: %s" % e.output)
@@ -525,7 +543,10 @@ class BenchmarkFramework(ABC):
             self.after_benchmark(config)
             # Save results
             log.info("Parsing results...")
-            bench_run_results = self.parse_results(stdout)
+            if self.second_command:
+                bench_run_results = self.parse_results(stdout, second_output=second_proc_output)
+            else:
+                bench_run_results = self.parse_results(stdout)
             bench_run = BenchRun(config, bench_run_results)
             results.append(bench_run)
             checkpoint_results(results_file, results)
---
+++ testing/cache_ext/bench_run_all_per_cgroup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+set -x
+set -o pipefail
+set -u
+
+# python3 bench_per_cgroup.py \
+#     --cpu 8 \
+#     --search-path /mydata/cgroup \
+#     --data-dir /mydata/cgroup/linuxes/linux \
+#     --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+#     --second-policy-loader ./page_cache_ext_mru_per_cgroup.out \
+#     --results-file ./results/final_per_cgroup_results.json \
+#     --leveldb-db /mydata/leveldb_myycsb_db \
+#     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+#     --benchmark ycsb_c
+
+python3 bench_per_cgroup.py \
+    --cpu 8 \
+    --search-path /mydata/cgroup \
+    --data-dir /mydata/cgroup/linuxes/linux \
+    --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+    --second-policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+    --results-file ./results/per_cgroup_baseline_results.json \
+    --leveldb-db /mydata/leveldb_myycsb_db \
+    --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+    --benchmark ycsb_c
+
+# python3 bench_per_cgroup.py \
+#     --cpu 8 \
+#     --search-path /mydata/cgroup \
+#     --data-dir /mydata/cgroup/linuxes/linux \
+#     --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+#     --second-policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+#     --results-file ./results/per_cgroup_both_lfu_results.json \
+#     --leveldb-db /mydata/leveldb_myycsb_db \
+#     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+#     --benchmark ycsb_c
+
+# python3 bench_per_cgroup.py \
+#     --cpu 8 \
+#     --search-path /mydata/cgroup \
+#     --data-dir /mydata/cgroup/linuxes/linux \
+#     --policy-loader ./page_cache_ext_mru_per_cgroup.out \
+#     --second-policy-loader ./page_cache_ext_mru_per_cgroup.out \
+#     --results-file ./results/per_cgroup_both_mru_results.json \
+#     --leveldb-db /mydata/leveldb_myycsb_db \
+#     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+#     --benchmark ycsb_c
+
+# python3 bench_per_cgroup.py \
+#     --cpu 8 \
+#     --search-path /mydata/cgroup \
+#     --data-dir /mydata/cgroup/linuxes/linux \
+#     --policy-loader ./page_cache_ext_sampling_per_cgroup.out \
+#     --second-policy-loader ./page_cache_ext_mru_per_cgroup.out \
+#     --results-file ./results/per_cgroup_split_results.json \
+#     --leveldb-db /mydata/leveldb_myycsb_db \
+#     --leveldb-temp-db /mydata/cgroup/leveldb_myycsb_db_temp \
+#     --benchmark ycsb_c
